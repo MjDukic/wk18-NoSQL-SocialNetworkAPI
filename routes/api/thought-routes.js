c5ab92e7d7db4546e3f72aad3cd585204373b0ea
MJ <mirjanadukicc@gmail.com>
2023-02-08 12:33:31 -0800

    working on thought routes, so far so good

--- routes/api/thought-routes.js
+++ routes/api/thought-routes.js
@@ -1,21 +1,46 @@
 
 const router = require('express').Router();
+const { restart } = require('nodemon');
 const { Thought, Reaction} = require('../../models')
 
 //TODO: ROUTE TO GET ALL THOUGHTS
+//needed to do a post before get to have something show up..why
 router.get('/', (req,res)=> {
-
-})
+    Thought.find({}, (err, thoughts) => {
+        if(err) {
+            res.status(400).json(err)
+        } else {
+            res.status(200).json(thoughts)
+        }
+    })
+});
 
 //TODO: ROUTE TO CREATE A NEW THOUGHT
+//username is required for this to work
 router.post('/', (req,res)=> {
-
+    Thought.create({
+        thoughtText: req.body.thoughtText,
+        username: req.body.username,
+        reactions: []
+    }, (err, thought) => {
+        if(err) {
+            res.status(400).json(err)
+        } else {
+            res.status(200).json(thought)
+        }
+    })
 });
 
 //TODO: ROUTE TO GET SINGLE THOUGHT BASED ON THOUGHT ID
 router.get('/:thoughtId', (req,res)=> {
-
-})
+    Thought.findOne({_id: req.params.thoughtId}, (err, thought) => {
+        if(err) {
+            res.status(400).json(err)
+        } else {
+            res.status(200).json(thought)
+        }
+    })
+});
 
 //TODO: ROUTE TO UPDATE A THOUGHT
 router.put('/', (req,res)=> {
