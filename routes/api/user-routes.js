90881c161ad21f249d4177ba1552b20d18a0c259
MJ <mirjanadukicc@gmail.com>
2023-02-10 13:10:44 -0800

    doing what I know, .put and .post causing problems, rest is working so far

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -4,27 +4,67 @@ const {User} = require("../../models")
 
 //TODO - ROUTE THAT GETS ALL THE USERS, include friends?
 router.get('/', (req,res)=> {
-
-})
+    User.find({}, (err, users) => {
+        if(err) {
+            res.status(400).json(err)
+        } else {
+            res.status(200).json(users)
+        }
+    })
+});
 
 //TODO - ROUTE THAT CREATES A NEW USER
+//to not include thoughts and friends? not working with them
 router.post('/', (req,res)=> {
+    User.create({
+        username: req.body.username,
+        email: req.body.email
+    }, (err, user) => {
+        if(err) {
+            res.status(400).json(err)
+        } else {
+            res.status(200).json(user)
+        }
+    })
 
 });
 
 //TODO - ROUTE THAT GETS A SINGLE USER BASED ON USER ID
 router.get('/:userId', (req,res) => {
+    User.findOne({_id: req.params.userId}, (err, user) => {
+        if(err) {
+            res.status(400).json(err)
+        } else {
+            res.status(200).json(user)
+        }
+    })
 
 })
 
 //TODO - ROUTE THAT UPDATES A SINGLE USER
+//no bueno, throws user and email in diff order
 router.put('/:userId', (req,res)=> {
-
+    User.findOneAndUpdate({_id: req.params.userId}, {
+        username: req.body.username,
+        email: req.body.email
+    }, (err, user) => {
+        if(err) {
+            res.status(400).json(err)
+        } else {
+            res.status(200).json(user)
+        }
+    })
 })
 
 //TODO - ROUTE THAT DELETES A SINGLE USER BASED ON USER ID
 router.delete('/:userId', (req,res)=> {
-
+    User.findOneAndDelete({_id: req.params.userId}, (err, user) => {
+        if(err) {
+            res.status(400).json(err)
+        } else {
+            res.status(200).json(user)
+        }
+    })
 });
 
 //TODO - ROUTE THAT ADDS A FRIEND TO A USER
